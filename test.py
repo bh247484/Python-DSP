c2ef89f44a243d8f4e4cd98f6f50b5fc788468ca
Brendan Huffman <bh247484@gmail.com>
2020-05-16 23:17:54 -0400

    imports, modules, and syntax experiments

--- test.py
+++ test.py
@@ -1,3 +1,27 @@
-import numpy
+from numpy import arange
+from matplotlib import pyplot
+from scipy import signal
 
-print(numpy.arange(0, 15))
+list = ["application", "bonanza", "pretty", "chard"]
+list.sort()
+
+x = arange(0, 50)
+
+# pyplot.plot(x)
+
+# pyplot.show()
+
+# window = signal.hann(51)
+
+# pyplot.plot(window)
+
+# pyplot.show()
+
+# print(arange(0, 50))
+
+# print(list)
+
+# for num in x:
+#     print(num + 5, "numby")
+
+print(list[0:4])
